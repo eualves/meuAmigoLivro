40749f81ebeb113a67ae74d54d85dc1b0d3e6b6d
Tamires <tamirespatrocinioo@gmail.com>
2021-06-30 18:42:16 -0300

    first commit

---
+++ View/Index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Index</title>
+</head>
+
+<body>
+
+    <header>
+        <a href="#" class="logo"><img src="img/logo.png" alt="logo"></a>
+        <ul class="navigation">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Livros</a></li>
+            <li><a href="#">Revista</a></li>
+            <li><a href="#">Quadrinhos</a></li>
+        </ul>
+
+        <button class="btn">Login</button>
+    </header>
+
+    <div class="banner">
+        <!-- <img src="" class="bg" alt="banner"> -->
+        <div class="content">
+
+            <div class="search">
+                <input type="text" placeholder="Search">
+                <button class="btn">Procurar</button>
+                <i class="fa fa-search" aria-hidden="true"></i>
+            </div>
+
+
+            <div class="listaAlfabetica">
+                <ul>
+                    <li><a href="#">A</a></li>
+                    <li><a href="#">B</a></li>
+                    <li><a href="#">C</a></li>
+                    <li><a href="#">D</a></li>
+                    <li><a href="#">E</a></li>
+                    <li><a href="#">F</a></li>
+                    <li><a href="#">G</a></li>
+                    <li><a href="#">H</a></li>
+                    <li><a href="#">I</a></li>
+                    <li><a href="#">J</a></li>
+                    <li><a href="#">K</a></li>
+                    <li><a href="#">L</a></li>
+                    <li><a href="#">M</a></li>
+                    <li><a href="#">N</a></li>
+                    <li><a href="#">O</a></li>
+                    <li><a href="#">P</a></li>
+                    <li><a href="#">Q</a></li>
+                    <li><a href="#">R</a></li>
+                    <li><a href="#">S</a></li>
+                    <li><a href="#">T</a></li>
+                    <li><a href="#">U</a></li>
+                    <li><a href="#">V</a></li>
+                    <li><a href="#">X</a></li>
+                    <li><a href="#">W</a></li>
+                    <li><a href="#">Y</a></li>
+                    <li><a href="#">Z</a></li>
+
+                </ul>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
